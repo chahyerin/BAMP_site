f79664da49f115f4e38504074bbb3d7bd280f57e
hyerincha <hyerin9610@naver.com>
2022-10-14 13:38:45 +0900

    인트로 화면 추가

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.getElementsByClassName('enter-btn')[0].addEventListener('click', function(){
+    document.getElementsByClassName('intro-page')[0].classList.add('hidden');
+    document.getElementsByClassName('main-page')[0].classList.add('show');
+})

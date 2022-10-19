2ea67e782885ca5bf99fb689e45517331207cd8f
hyerincha <hyerin9610@naver.com>
2022-10-19 19:52:37 +0900

    인트로 페이지 기능 완료: 라디오 버튼, 프로필 선택 안하면 에러 메세지, 메인 페이지에 선택한 프로필 이름 표시

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-document.getElementsByClassName('enter-btn')[0].addEventListener('click', function(){
-    document.getElementsByClassName('intro-page')[0].classList.add('hidden');
-    document.getElementsByClassName('main-page')[0].classList.add('show');
+$('.enter-btn').click(function(e){
+    var listVar = $('input[name=person]:checked').val();
+    
+    if (listVar === '밤프인' || listVar === '예비 밤프인') {
+        document.querySelector('.intro-page').classList.add('hidden');
+        document.querySelector('.main-page').classList.add('show');  
+        document.querySelector('.selected-profile').innerText = listVar;
+    } else {
+        e.preventDefault();
+        $('.message-profile').addClass('error-show');
+    }
 })
 
+//채팅 스크롤 기능 
 $(window).scroll(function(){
     var height = $(window).scrollTop();
     console.log(height);
@@ -39,31 +48,4 @@ $(window).scroll(function(){
         document.querySelector('.chat10').style.opacity = "1";
     } else { document.querySelector('.chat10').style.opacity = "0"; }
 
-})
-
-$(window).scroll(function(){
-    var height = $(window).scrollTop();
-    console.log(height);
-
-    // 1080~1625까지 스크롤 바  내리면 
-    // 첫째 카드의 opacity 1~0으로 서서히 변경해줘
-    var FirstCardOpacity = (-1/545) * height + 1625/545;
-    var SecondCardOpacity = (-1/542) * height + 2378/542;
-    // 같은 높이인데 스케일을 1~0.8로 서서히 변경해줘
-    var FirstCardScale = (-1/2725) * height + 761/545;
-    var SecondCardScale = (-1/2490) * height + 1.7;
-    
-    $('.card-box').eq(0).css('opacity', FirstCardOpacity);
-    $('.card-box').eq(1).css('opacity', SecondCardOpacity);
-
-    // $('.card-box').eq(0) 의 스케일 값이 1 이하일 때만 아래 코드를 실행
-    // 1. $('.card-box').eq(0) 의 현재 스케일 값 구하는 법 찾기
-    // 2. if 문으로 현재 스캐일 값이 1이하일 때 아래 코드 실행
-
-    if(FirstCardScale <= 1) {
-        $('.card-box').eq(0).css('transform', `scale(${FirstCardScale})`);
-    }
-    if(SecondCardScale <= 1) {
-        $('.card-box').eq(1).css('transform', `scale(${SecondCardScale})`);
-    }
 })

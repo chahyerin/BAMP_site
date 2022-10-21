9a3ee69d6e5fc95f945b8177c22d913a68f0477a
hyerincha <hyerin9610@naver.com>
2022-10-21 19:54:28 +0900

    채팅 스크롤 아닌 초에 따라 나타나는 것으로 수정

--- js/main.js
+++ js/main.js
@@ -11,44 +11,39 @@ $('.enter-btn').click(function(e){
     }
 })
 
-//채팅 스크롤 기능 
-$(window).scroll(function(){
-    var height = $(window).scrollTop();
-    console.log(height);
-
-    //스크롤 높이가 115면 chat1의 opacity를 1로 변경해줘
-    if (height >= 115) {
-        document.querySelector('.chat1').style.opacity = "1";
-    } else { document.querySelector('.chat1').style.opacity = "0"; }
-    if (height >= 198) {
-        document.querySelector('.chat2').style.opacity = "1";
-    } else { document.querySelector('.chat2').style.opacity = "0"; }
-    if (height >= 270) {
-        document.querySelector('.chat3').style.opacity = "1";
-    } else { document.querySelector('.chat3').style.opacity = "0"; }
-    if (height >= 410) {
-        document.querySelector('.chat4').style.opacity = "1";
-    } else { document.querySelector('.chat4').style.opacity = "0"; }
-    if (height >= 505) {
-        document.querySelector('.chat5').style.opacity = "1";
-    } else { document.querySelector('.chat5').style.opacity = "0"; }
-    if (height >= 597) {
-        document.querySelector('.chat6').style.opacity = "1";
-    } else { document.querySelector('.chat6').style.opacity = "0"; }
-    if (height >= 703) {
-        document.querySelector('.chat7').style.opacity = "1";
-    } else { document.querySelector('.chat7').style.opacity = "0"; }
-    if (height >= 796) {
-        document.querySelector('.chat8').style.opacity = "1";
-    } else { document.querySelector('.chat8').style.opacity = "0"; }
-    if (height >= 885) {
-        document.querySelector('.chat9').style.opacity = "1";
-    } else { document.querySelector('.chat9').style.opacity = "0"; }
-    if (height >= 980) {
-        document.querySelector('.chat10').style.opacity = "1";
-    } else { document.querySelector('.chat10').style.opacity = "0"; }
-
-})
+//채팅 보여주기 
+$('.enter-btn').click(function(e){
+    setTimeout(function Timer () {
+        document.querySelector('.chat1').classList.add('chat-show'); 
+    }, 400);
+    setTimeout(function Timer () {
+        document.querySelector('.chat2').classList.add('chat-show'); 
+    }, 800);
+    setTimeout(function Timer () {
+        document.querySelector('.chat3').classList.add('chat-show'); 
+    }, 1700);
+    setTimeout(function Timer () {
+        document.querySelector('.chat4').classList.add('chat-show'); 
+    }, 3400);
+    setTimeout(function Timer () {
+        document.querySelector('.chat5').classList.add('chat-show'); 
+    }, 4000);
+    setTimeout(function Timer () {
+        document.querySelector('.chat6').classList.add('chat-show'); 
+    }, 4600);
+    setTimeout(function Timer () {
+        document.querySelector('.chat7').classList.add('chat-show'); 
+    }, 5600);
+    setTimeout(function Timer () {
+        document.querySelector('.chat8').classList.add('chat-show'); 
+    }, 6400);
+    setTimeout(function Timer () {
+        document.querySelector('.chat9').classList.add('chat-show'); 
+    }, 7000);
+    setTimeout(function Timer () {
+        document.querySelector('.chat10').classList.add('chat-show'); 
+    }, 8000);
+});
 
 //앵커버튼 focus 설정
 for (let i = 0; i < $('.anchor-btn').length; i++){
